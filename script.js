8cc19656d86aae59217f9cff7fc1d3a386e1f8b2
Cristian Ramiro Rios <rios_cristian@yahoo.com.ar>
2022-01-25 20:48:41 -0300

    cambios finales del proyeto
    agregamos pie de pagina
    cambiamos colores de títulos y párrafos
    agregamos alerta en botón de copiado
    se eliminas id sin usos

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 
 function fcifrado(){
-    const capturarTexto = document.getElementById("textoEntrada").value;
-    const textoCifrado = capturarTexto.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
-    document.getElementById("textoSalida").value =textoCifrado
-    return false
-     
-    }
+  const capturarTexto = document.getElementById("textoEntrada").value;
+  if (capturarTexto.match(/[^a-z\s]/)) {
+    return alert("No deben ser utilizados letras con acentos ni caracteres especiales");
+}
+  const textoCifrado = capturarTexto.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
+  document.getElementById("textoSalida").value =textoCifrado
+  return false
+}
 
 
 function fdescifrado(){
-    const capturarTexto = document.getElementById("textoEntrada").value;
-    const descifrado = capturarTexto.replace(/ai/gi, 'a').replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
-    document.getElementById("textoSalida").value = descifrado
-    return false
+  const capturarTexto = document.getElementById("textoEntrada").value;
+  if (capturarTexto.match(/[^a-z\s]/)) {
+    return alert("No deben ser utilizados letras con acentos ni caracteres especiales");
+}
+  const descifrado = capturarTexto.replace(/ai/gi, 'a').replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
+  document.getElementById("textoSalida").value = descifrado
+  return false
     
-    }
+}
     
 function fcopiarTexto(){
   const copiarTexto = document.getElementById("textoSalida");
   copiarTexto.select();
   document.execCommand("Copy");
-  navigator.clipboard.writeText(copiarTexto.value);
-  const copia = copiarTexto.value;
+  alert("Mensaje copiado");
   return false
 }
 
 /*carga del archivo txt*/
 function mostrarContenido(contenido) {
-    const mostrarcuadro = document.getElementById("textoEntrada");
-    mostrarcuadro.value = contenido;
-  }
+  const mostrarcuadro = document.getElementById("textoEntrada");
+  mostrarcuadro.value = contenido;
+}
 function leerArchivo(e) {
   const archivo = e.target.files[0];
   if (!archivo) {
@@ -46,21 +50,21 @@ function leerArchivo(e) {
 document.querySelector('#archivo1')
   .addEventListener('change', leerArchivo, false);
 
-/*Creamos eun archivo txt*/
-  function exportar (data, fileName) {
-    const a = document.createElement("a");
-    const contenido = data,
-        blob = new Blob([contenido], {type: "octet/stream"}),
-        url = window.URL.createObjectURL(blob);
-    a.href = url;
-    a.download = fileName;
-    a.click();
-    window.URL.revokeObjectURL(url);
-    
-  };
+/*Creamos un archivo txt con el texto del cuadro textoSalida*/
+function exportar (data, fileName) {
+  const a = document.createElement("a");
+  const contenido = data,
+      blob = new Blob([contenido], {type: "octet/stream"}),
+      url = window.URL.createObjectURL(blob);
+  a.href = url;
+  a.download = fileName;
+  a.click();
+  window.URL.revokeObjectURL(url);
+  
+};
 
-  function fcrearTxt() {
-    const data = document.getElementById("textoSalida").value;
-    const nombreArchivo = 'mensaje.txt';
-    exportar(data, nombreArchivo);
-  }
+function fcrearTxt() {
+  const data = document.getElementById("textoSalida").value;
+  const nombreArchivo = 'mensaje.txt';
+  exportar(data, nombreArchivo);
+}
